949a3a2ffc45612b41f25f680b16586adb741f79
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-11-30 21:01:02 +0530

    Create Card.stories.js

---
+++ web/src/components/Card/Card.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ReviewCard from './Card';
+
+storiesOf('ReviewCard', module)
+    .add('default', () => <ReviewCard></ReviewCard>);
